40a2e6db94221bd32832d25236e8c835c613050a
myname21-meet <myname21@meet.mit.edu>
2019-11-07 12:08:51 -0500

    done

---
+++ databases.py
@@ -0,0 +1,63 @@
+from model import Base, Product
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+engine = create_engine('sqlite:///database.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+def add_product(Name, Price, Picture_Link, Description):
+    Product_object = Product(
+        Name=Name,
+        Price=Price,
+        Picture_Link=Picture_Link,
+        Description=Description)
+    session.add(Product_object)
+    session.commit()
+
+def update_Price(name, Price):
+   Product_object = session.query(
+       Product).filter_by(
+       Name=name).first()
+   student_object.Price = Price
+   session.commit()
+
+def update_Price(name, Description):
+   Product_object = session.query(
+       Product).filter_by(
+       Name=name).first()
+   student_object.Description = Description
+   session.commit()
+
+def update_Picture_Link(name, Picture_Link):
+   Product_object = session.query(
+       Product).filter_by(
+       Name=name).first()
+   student_object.Picture_Link = Picture_Link
+   session.commit()
+ 
+def delete_student(name):
+   session.query(Product).filter_by(
+       Name=name).delete()
+   session.commit()
+
+def query_all():
+   Products = session.query(
+      Product).all()
+   return Products
+
+def query_by_name(name):
+   product = session.query(
+       Product).filter_by(
+       Name=name).first()
+   return product
+
+def Add_To_Cart(ProductID):
+    Cart_object = Cart(
+        ProductID=ProductID)
+    session.add(Cart_object)
+    session.commit()
---
+++ model.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, Float, Boolean, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
+
+class Product(Base):
+   __tablename__ = 'Product'
+   id = Column(Integer, primary_key=True)
+   Name = Column(String)
+   Price = Column(Float)
+   Picture_Link = Column(String)
+   Description=(String)
+
+
+class Cart(Base):
+   __tablename__ = 'Cart'
+   ProductID = Column(Integer, primary_key=True)
+
+
